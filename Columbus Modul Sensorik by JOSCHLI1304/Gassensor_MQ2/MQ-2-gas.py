7587c797296cb285967c9c48d930e5733d37931b
joschli1304 <45271027+JOSCHLI1304@users.noreply.github.com>
2019-04-08 14:47:47 +0200

    Update MQ-2-gas.py

--- Columbus Modul Sensorik by JOSCHLI1304/Gassensor_MQ2/MQ-2-gas.py
+++ Columbus Modul Sensorik by JOSCHLI1304/Gassensor_MQ2/MQ-2-gas.py
@@ -2,22 +2,22 @@
 from mq import *
 import sys, time
 
-#try:
-#    print("Press CTRL+C to abort.")
+try:
+    print("Press CTRL+C to abort.")
     
-#    mq = MQ();
-#    while True:
-#        perc = mq.MQPercentage()
-#        sys.stdout.write("\r")
-#        sys.stdout.write("\033[K")
-#        sys.stdout.write("LPG: %g ppm, CO: %g ppm, Smoke: %g ppm" % (perc["GAS_LPG"], perc["CO"], perc["SMOKE"]))
-#        sys.stdout.flush()
-#        time.sleep(0.1)
+    mq = MQ();
+    while True:
+        perc = mq.MQPercentage()
+        sys.stdout.write("\r")
+        sys.stdout.write("\033[K")
+        sys.stdout.write("LPG: %g ppm, CO: %g ppm, Smoke: %g ppm" % (perc["GAS_LPG"], perc["CO"], perc["SMOKE"]))
+        sys.stdout.flush()
+        time.sleep(0.1)
 
-#except:
-#    print("\nAbort by user")
-   # e = sys.exc_info()[0]
-   # print(e)
+except:
+    print("\nAbort by user")
+    e = sys.exc_info()[0]
+    print(e)
    
 def Calibrate():
         mq = MQ();
